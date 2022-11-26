96902c82fb0aa567f03427911fa67f7824bac707
XaviBarri <34490500+XaviBarri@users.noreply.github.com>
2022-11-26 17:39:38 +0100

    First console App from .NET course

---
+++ MyApp/Alumno.cs
@@ -0,0 +1,13 @@
+namespace AppConsole {
+
+    public class Alumno : Persona
+    {
+        public string[] ListaAsignaturas { get; set; }
+        public string Aula { get; set; }
+
+        public override void MostrarNombre()
+        {
+            Console.WriteLine("El nombre es " + Nombre + " " + Apellidos);
+        }
+    }
+}
